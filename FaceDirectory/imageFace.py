7322fdbeb510fd31eb56b696b64a738c2af23c90
taner <tanerozer16@gmail.com>
2023-08-18 14:07:50 +0300

    Create imageFace.py

---
+++ FaceDirectory/imageFace.py
@@ -0,0 +1,34 @@
+import cv2
+import os
+
+res = cv2.imread("../resimler/soso.jpg")
+yuzcascade = cv2.CascadeClassifier('../haarcascade_frontalface_default.xml')
+
+adet = 0
+
+id = input("id girin:")
+name = input("isim giriniz")
+yol = 'yuzverilerim/'+ name + '/'
+
+
+while True:
+    griton = cv2.cvtColor(res, cv2.COLOR_BGR2GRAY)
+    yuzler = yuzcascade.detectMultiScale(griton, scaleFactor=1.3, minNeighbors=5, minSize=(100,100))
+
+    for(x, y, w, h) in yuzler:
+        yuz = cv2.rectangle(res, (x , y), (x + w, y + h), (0, 255, 0), 3)
+        adet += 1
+        cv2.imwrite(yol + str(id) + "_" + str(adet) + ".jpg",
+                griton[y:y + h, x:x + w])
+        cv2.imshow("yuzler", res)
+
+    if (cv2.waitKey(30) & 0xFF == ord('q')):
+        break
+    elif adet>99:
+        break
+
+
+print("Yüz verileri kaydedildi.\nProgram Sonlanıyor...!")
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
